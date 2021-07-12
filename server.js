2dd8cf09a868436e4d43ebf7aab5212a1eadcf96
giyun-cloud <nangiyoon@gmail.com>
2021-07-12 11:14:31 +0900

    3.3 POC API

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const movies = [
+  {
+    title: "hello",
+    year: 2021,
+  },
+];
+
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie(id: Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => movies,
+    movie: (_, { id }) => {
+      const result = movies.filter((m, i) => i === id);
+      return result[0];
+    },
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      movies.push({ title });
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      const result = movies.filter((m) => m.title !== title);
+      movies = result;
+      return true;
+    },
   },
 };
 
