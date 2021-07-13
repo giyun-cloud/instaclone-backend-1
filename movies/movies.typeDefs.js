ec02a34f607e973fdf555b582c8e36277287b07c
giyun-cloud <nangiyoon@gmail.com>
2021-07-13 12:11:52 +0900

    3.9 Architecture part One

---
+++ movies/movies.typeDefs.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-server";
+
+export const typeDefs = gql`
+  type Movie {
+    id: Int!
+    createdAt: String!
+    updatedAt: String!
+    year: Int!
+    genre: String
+    title: String!
+  }
+  type Query {
+    movies: [Movie]
+    movie(id: Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
+  }
+`;
