1bcc120d4864e89c1a22400f25e2e15b834b7183
giyun-cloud <nangiyoon@gmail.com>
2021-07-19 07:02:56 +0900

    4.7 updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,36 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
-    editProfile: (_, { username, password }) => {},
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password },
+    ) => {
+      let uglyPassword = null;
+      password && (uglyPassword = await bcrypt.hash(password, 10));
+      const updatedUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not find user",
+        };
+      }
+    },
   },
 };
