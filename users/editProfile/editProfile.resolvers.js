174872eaff05e6ad6ef4df485ae531ab99008048
giyun-cloud <nangiyoon@gmail.com>
2021-07-21 11:29:50 +0900

    4.13 Recap

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,41 +1,38 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
 import { protectedResolver } from "../users.utils";
-
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password },
+  { loggedInUser },
+) => {
+  let uglyPassword = null;
+  password && (uglyPassword = await bcrypt.hash(password, 10));
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not find user",
+    };
+  }
+};
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (
-        _,
-        { firstName, lastName, username, email, password },
-        { loggedInUser, protectResolver },
-      ) => {
-        protectResolver(loggedInUser);
-        let uglyPassword = null;
-        password && (uglyPassword = await bcrypt.hash(password, 10));
-        const updatedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            ...(uglyPassword && { password: uglyPassword }),
-          },
-        });
-        if (updatedUser) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "Could not find user",
-          };
-        }
-      },
-    ),
+    editProfile: protectedResolver(resolverFn),
   },
 };
