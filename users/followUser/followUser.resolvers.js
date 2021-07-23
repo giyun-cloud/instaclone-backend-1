4977eb8e1d6947237575f4cf035d67ef06e0ada0
giyun-cloud <nangiyoon@gmail.com>
2021-07-23 18:30:27 +0900

    4.21 Following User

---
+++ users/followUser/followUser.resolvers.js
@@ -0,0 +1,28 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Mutation: {
+    followUser: protectedResolver(async (_, { username }, { loggedInUser }) => {
+      const ok = await client.user.findUnique({
+        where: {
+          username,
+        },
+      });
+      if (!ok) return { ok: false, error: "There is no such username" };
+      await client.user.update({
+        where: { id: loggedInUser.id },
+        data: {
+          following: {
+            connect: {
+              username,
+            },
+          },
+        },
+      });
+      return {
+        ok: true,
+      };
+    }),
+  },
+};
